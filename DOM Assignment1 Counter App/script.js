a8cbbde2ba1eb77c9a6b5f409248082b2a5e093a
vguru shetty <78691843+VguruShetty@users.noreply.github.com>
2023-12-06 20:20:44 +0530

    Update script.js

--- DOM Assignment1 Counter App/script.js
+++ DOM Assignment1 Counter App/script.js
@@ -1,4 +1,4 @@
-// getting html ele,ments
+// getting html elements
 const btn1=document.getElementById("btn1");
 const btn2=document.getElementById("btn2");
 const btn3=document.getElementById("btn3");
